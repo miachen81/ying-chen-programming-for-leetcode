aa03a438fbfb83ff9076f1c2e8b21a50b3bc701a
丁惠春 <dinghuichun@teimegumiharus-macbook.local>
2016-06-12 13:43:25 -0400

    Initial Commit

---
+++ leetcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  leetcode
+//
+//  Created by 丁惠春 on 2016-06-12.
+//  Copyright © 2016 Mia Chen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
